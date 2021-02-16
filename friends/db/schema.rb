37d39f03405a8de9abd55a27c06d7be52084b2b9
Vaibhav Rawat <vasurawat1998@gmail.com>
2021-02-16 21:46:16 +0530

    UX improvement

---
+++ friends/db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2021_02_14_164115) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
